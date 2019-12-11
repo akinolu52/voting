1cab70569257bb653de260718b0448aea7910132
Akinyemi Olumide Emmannuel <akinolu52@gmail.com>
2019-12-11 14:50:58 +0100

    db conn

--- includes/connection.php
+++ includes/connection.php
@@ -1,7 +1,15 @@
 <?php
-     define('HOST','localhost');
-     define('USER','root');
-     define('PASS','root');
-     define('DB','evoting');
+     if ($_SERVER['SERVER_NAME'] === "localhost") {
+          define('HOST','localhost');
+          define('USER','root');
+          define('PASS','root');
+          define('DB','evoting');
+     } else {
+          define('HOST','us-cdbr-iron-east-05.cleardb.net');
+          define('USER','b6a36022cae1da');
+          define('PASS','34ad7e1a');
+          define('DB','heroku_6dbf2da12e8a724');
+     }
+     
      $db = mysqli_connect(HOST,USER,PASS,DB) or die('could not connect to database');
 ?>
